b93d57f2895dae2e2ef37fe2d8379cbe318e2799
n-zeplo <nzeplowitz@gmail.com>
2014-09-25 23:06:28 -0400

    Organize Files and Removew Practice Client Key

---
+++ build/js/dropbox_oauth.js
@@ -0,0 +1,14 @@
+var client = new Dropbox.Client({ key: CLIENT_KEY });
+
+client.authDriver(new Dropbox.AuthDriver.ChromeExtension({
+  receiverPath: "html/chrome_oauth_receiver.html",
+
+  })
+);
+
+client.authenticate(function(error, client) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(client.credentials());
+});
