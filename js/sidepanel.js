a14da440ed2835941c3e5764f983d771ab114084
stefankp <skp87@me.com>
2014-09-28 21:52:04 -0400

    Add 'onchange' event listener for chrome storage in sidepanel.js

--- js/sidepanel.js
+++ js/sidepanel.js
@@ -1,11 +1,15 @@
-//TODO Move Into Background to get Current URL of Active Tab
-function getCurrentUrl() {
-  var currentUrl;
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-      currentUrl = tabs[0].url;
-      console.log(currentUrl);
+$(document).ready(function(){
+
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      console.log('CHANGED STORAGE: key "%s" in namespace "%s" changed. ' +
+                  'Old: "%s", New: "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
   });
 
-}
-
-
+});
