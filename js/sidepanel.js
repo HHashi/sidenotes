b2f19587f9fb71dddec36b7a390a74d3fededb25
n-zeplo <nzeplowitz@gmail.com>
2014-09-29 17:38:11 -0400

    Refactor

--- js/sidepanel.js
+++ js/sidepanel.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
     function setIframeData() {
       var chromeStorage = {};
-      chromeStorage['iNote'] = { 'url': currentLocation, 'body': noteBody, 'date': JSON.stringify(new Date()) }
+      chromeStorage['iNote'] = { 'url': currentLocation, 'body': noteBody, 'date': JSON.stringify(new Date()) };
       chrome.storage.local.set(chromeStorage, function() {});
     };
     setIframeData();
