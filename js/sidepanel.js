5d8eadbb59d50e125caa87112fd5e43ff1e237b4
stefankp <skp87@me.com>
2014-09-28 21:58:52 -0400

    Save 'iframeSideNote'-object to chrome storage

--- js/sidepanel.js
+++ js/sidepanel.js
@@ -1,3 +1,5 @@
+var currentLocation = window.location.hash.slice(1).split('#')[0];
+
 $(document).ready(function(){
 
   chrome.storage.onChanged.addListener(function(changes, namespace) {
@@ -15,5 +17,19 @@ $(document).ready(function(){
   // Activate textarea
   $('#textarea').attr('contenteditable','true');
   $('#textarea').focus();
+
+  // Create note from textarea content
+  $('#save-note').on('click', function(){
+
+    var noteBody = $('#textarea').text();
+
+    function setIframeData() {
+      var chromeStorage = {};
+      chromeStorage['iframeSideNote'] = { 'url': currentLocation, 'body': noteBody, 'date': new Date() }
+      chrome.storage.local.set(chromeStorage, function() {});
+    };
+    setIframeData();
+
+  });
 
 });
