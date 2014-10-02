dd65444e55baf209bedc22dc3eea991b1add6c94
n-zeplo <nzeplowitz@gmail.com>
2014-10-02 10:48:44 -0400

    Fix Bug in the Background

--- js/background.js
+++ js/background.js
@@ -147,7 +147,7 @@ function initDatastore(callback){
 
     chrome.storage.onChanged.addListener(function(changes, namespace) {
       var hashKey = Object.keys(changes)[0];
-      if(changes[hashKey]['newValue']['url'] && changes[hashKey]['newValue']['body']){
+      if(typeof(changes[hashKey]['newValue']['url']) !== 'undefined' && typeof(changes[hashKey]['newValue']['body']) !== 'undefined'){
         var existingRecord = currentTable.query({url: changes[hashKey]['newValue']['url'] });
         datastoreController.updateOrAddRecord(changes, existingRecord[0], hashKey);
       }
