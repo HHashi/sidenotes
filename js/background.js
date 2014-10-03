2eba223f7e84c016a73f201862551fe5a1b1a724
n-zeplo <nzeplowitz@gmail.com>
2014-10-02 21:11:32 -0400

    Fix Bugs

--- js/background.js
+++ js/background.js
@@ -81,7 +81,7 @@ appController = {
 datastoreController = {
   updateOrAddRecord: function(newNote, pastNote, hashKey){
     var newNoteData = this.makeRecord(newNote[hashKey]);
-    if(pastNote) {
+    if(pastNote && pastNote.get('body') !== newNote[hashKey]['newValue']['body']) {
       pastNote.update(newNoteData);
     } else {
       currentTable.insert(newNoteData);
@@ -97,7 +97,7 @@ datastoreController = {
   },
   setRemoteNoteToLocalStorage: function(newRemoteNote) {
     chrome.storage.local.get(null, function(result){
-        var newLocalNotes = datastoreController.mergeNotes([newRemoteNote]);
+        var newLocalNotes = datastoreController.mergeNotes([newRemoteNote], result);
     });
   },
   syncRemoteStorage: function(currentTable){
@@ -113,7 +113,6 @@ datastoreController = {
     if(chromeLocalRecords){
       for (var i=0;i<datastoreRecords.length;i++) {
         var noteKey = hashConverter.hex(datastoreRecords[i].get('url'));
-        console.log(chromeLocalRecords);
         var localMatchNote = chromeLocalRecords[noteKey];
         var newNote = {};
         if(localMatchNote){
